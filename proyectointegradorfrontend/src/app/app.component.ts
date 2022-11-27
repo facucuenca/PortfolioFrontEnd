83c77f6cbf55a841762adcaf318a01da5d90f297
Facu Cuenca <facucuenca@outlook.com>
2022-11-27 10:22:01 -0300

    creando header por segunda vez

--- proyectointegradorfrontend/src/app/app.component.ts
+++ proyectointegradorfrontend/src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proyectointegradorfrontend';
+  
 }
--- proyectointegradorfrontend/src/app/app.module.ts
+++ proyectointegradorfrontend/src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercadeComponent } from './components/acercade/acercade.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoAPComponent
+    LogoAPComponent,
+    SocialComponent,
+    BannerComponent,
+    AcercadeComponent
   ],
   imports: [
     BrowserModule
